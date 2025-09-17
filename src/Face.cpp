0cf541dfd7e162a31606eabd4aa10cdd31073335
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-09-17 01:08:31 -0500

    Initialize internal face sprite

--- src/Face.cpp
+++ src/Face.cpp
@@ -4,6 +4,7 @@
 void InitFace(face_t* face, float x, float y, float dir, GLuint texture) {
 	face->x = x;
 	face->y = y;
+	InitSprite(&face->sprite);
 	face->sprite.texture = texture;
 	DirectX::XMScalarSinCos(&face->sprite.dirs, &face->sprite.dirc, dir);
 }
