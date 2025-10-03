5f59a83b1ffaa517ccd715eead4192cf506bf4a3
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-10-02 23:28:04 -0500

    Use templates instead for this "fixed" list thing

--- include/List.hpp
+++ include/List.hpp
@@ -1,35 +1,186 @@
 #ifndef	LIST_INCLUDED
 #define LIST_INCLUDED
 
-#include "stdlib.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 // Single list
+template<typename T>
 struct node_t {
 	bool active;
-	void* data;
-	struct node_t* next;
+	T data;
+	struct node_t<T>* next;
 };
 
-typedef struct {
-	struct node_t* head;
-	struct node_t* nodes;
+template<typename T, int max_space>
+struct list_t {
+	struct node_t<T>* head;
+	struct node_t<T> nodes[max_space];
 	int count;
 	int last_search;
-	int max;
-} list_t;
+};
 
-void InitSingleList(list_t* list, int max);
+template<typename T, int max_space>
+void InitSingleList(list_t<T, max_space>* list) {
+	memset(list, 0, sizeof(list_t<T, max_space>));
+	list->head = list->nodes;
+	list->last_search = 0;
+	list->count = 0;
+}
 
-void DestroySingleList(list_t* list);
+template<typename T, int max_space>
+void DestroySingleList(list_t<T, max_space>* list) {
+	free(list->nodes);
+	memset(list, 0, sizeof(list_t<T, max_space>));
+}
 
-node_t* SLPushItem(list_t* list, void* data);
+template<typename T, int max_space>
+node_t<T>* SLPushItem(list_t<T, max_space>* list, T data) {
+	if (list->count >= max_space) return nullptr;
 
-void SLPopItem(list_t* list);
+	int max = max_space, max2 = max_space;
+	node_t<T>* node = nullptr;
+	int i = list->last_search;
+	int start = i;
+search:
+	// Look for space
+	for (; i < max2; i++) {
+		if (!list->nodes[i].active) {
+			// Free node
+			node = list->nodes + i;
 
-void SLRemoveItemAt(list_t* list, int pos);
+			node->active = true;
+			node->data = data;
+			node->next = nullptr;
+			list->last_search = (i + 1 >= max) ? 0 : i + 1;
 
-void SLReset(list_t* list);
+			goto look_for_node;
+		}
+	}
+
+	i = 0;
+	max2 = start;
+	goto search;
+
+look_for_node:
+	// Look for the last node
+	for (node_t<T>* idx = list->head; ; ) {
+		if (list->count == 0) {
+			break;
+		}
+		else if (idx->next == 0) {
+			idx->next = node;
+			break;
+		}
+		idx = idx->next;
+	}
+
+	// Finally node is found and that stuff
+	list->count++;
+	return node;
+}
+
+template<typename T, int max_space>
+node_t<T>* SLPushItem(list_t<T, max_space>* list) {
+	if (list->count >= max_space) return nullptr;
+
+	int max = max_space, max2 = max_space;
+	node_t<T>* node = nullptr;
+	int i = list->last_search;
+	int start = i;
+search:
+	// Look for space
+	for (; i < max2; i++) {
+		if (!list->nodes[i].active) {
+			// Free node
+			node = list->nodes + i;
+
+			node->active = true;
+			node->data = { };
+			node->next = nullptr;
+			list->last_search = (i + 1 >= max) ? 0 : i + 1;
+
+			goto look_for_node;
+		}
+	}
+
+	i = 0;
+	max2 = start;
+	goto search;
+
+look_for_node:
+	// Look for the last node
+	for (node_t<T>* idx = list->head; ; ) {
+		if (list->count == 0) {
+			break;
+		}
+		else if (idx->next == 0) {
+			idx->next = node;
+			break;
+		}
+		idx = idx->next;
+	}
+
+	// Finally node is found and that stuff
+	list->count++;
+	return node;
+}
+
+template<typename T, int max_space>
+void SLPopItem(list_t<T, max_space>* list) {
+	if (list->count <= 0) return;
+	node_t<T>* prev = 0;
+	for (node_t<T>* node = list->head; ; ) {
+		if (node->next == 0) {
+			if (prev == 0) {
+				node->active = false;
+				break;
+			}
+			else {
+				node->active = false;
+				prev->next = 0;
+				break;
+			}
+		}
+		prev = node;
+		node = node->next;
+	}
+	list->count--;
+}
+
+template<typename T, int max_space>
+void SLRemoveItemAt(list_t<T, max_space>* list, int pos) {
+	if (pos >= list->count || pos < 0) return;
+	node_t<T>* prev = 0;
+	int i = 0;
+	for (node_t<T>* node = list->head; ; ) {
+		if (i == pos) {
+			if (prev == 0) {
+				node->active = false;
+				list->head = node->next;
+				break;
+			}
+			else {
+				node->active = false;
+				prev->next = node->next;
+				break;
+			}
+		}
+		prev = node;
+		node = node->next;
+		i++;
+	}
+	list->count--;
+}
+
+template<typename T, int max_space>
+void SLReset(list_t<T, max_space>* list) {
+	memset(list->nodes, 0, sizeof(node_t<T>) * max_space);
+	list->count = 0;
+	list->head = list->nodes;
+	list->last_search = 0;
+}
 
 // Double Linked List
 struct dual_node_t {
--- test/list.cpp
+++ test/list.cpp
@@ -2,21 +2,22 @@
 #include <stdint.h>
 
 int main() {
-	list_t list;
-	InitSingleList(&list, 10);
+	list_t<int, 10> list;
+	InitSingleList(&list);
 
 	for (int i = 1; i <= 10; i++) {
-		SLPushItem(&list, (void*)(uint64_t)(i * 10));
+		node_t<int>* n = SLPushItem(&list);
+		n->data = i * 10;
 	}
 
 	SLRemoveItemAt(&list, 3);
 	SLPopItem(&list);
 
-	for (node_t* node = list.head; node; node = node->next) {
-		printf("Node val: %d\n", (uint64_t)node->data);
+	for (node_t<int>* node = list.head; node; node = node->next) {
+		printf("Node val: %d\n", node->data);
 	}
 
-	for (node_t* node = list.head; node; node = node->next) {
+	for (node_t<int>* node = list.head; node; node = node->next) {
 		printf("Node this: 0x%p\n", node);
 		printf("Node next: 0x%p\n", node->next);
 	}
