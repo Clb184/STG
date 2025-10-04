0882f3ceb90c5c6d3a174a06eceb8cba78bd878a
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-10-04 00:33:54 -0500

    Make texture manager completely global

--- include/TextureManager.hpp
+++ include/TextureManager.hpp
@@ -3,15 +3,17 @@
 
 #include <OpenGL/Texture.h>
 
+#define MAX_TEXTURES 128
+
 struct texture_manager_t {
 	int cnt; // How many textures
-	texture_metric_t* texture_metrics; // Metrics for textures
-	GLuint* textures; // Texture ids for OpenGL
+	texture_metric_t texture_metrics[MAX_TEXTURES; // Metrics for textures
+	GLuint textures[MAX_TEXTURES]; // Texture ids for OpenGL
 };
 
-void InitTextureManager(texture_manager_t* tex_manager, int max);
-GLuint RegisterTexture(texture_manager_t* tex_manager, const char* source);
-void DestroyTextureManager(texture_manager_t* tex_manager);
+void InitTextureManager();
+GLuint RegisterTexture(const char* source);
+void DestroyTextureManager();
 
 extern texture_manager_t tex_manager;
 
--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -5,29 +5,25 @@
 
 texture_manager_t tex_manager; // Hold all texture data
 
-void InitTextureManager(texture_manager_t* tex_manager, int max) {
-	assert(nullptr != tex_manager);
-	tex_manager->cnt = 0;
-	tex_manager->textures = (GLuint*)malloc(sizeof(GLuint) * max);
-	memset(tex_manager->textures, 0xffffffff, sizeof(GLuint) * max);
-	tex_manager->texture_metrics = (texture_metric_t*)calloc(max, sizeof(texture_metric_t));
+void InitTextureManager() {
+	tex_manager.cnt = 0;
+	memset(tex_manager.textures, 0xffffffff, sizeof(GLuint) * MAX_TEXTURES);
+	memset(tex_manager.texture_metrics, 0x00, sizeof(texture_metric_t) * MAX_TEXTURES);
 }
 
-GLuint RegisterTexture(texture_manager_t* tex_manager, const char* source) {
-	assert(nullptr != tex_manager);
+GLuint RegisterTexture(const char* source) {
 
-	int pos = tex_manager->cnt;
+	int pos = tex_manager.cnt;
 	GLuint ret = -1;
 
-	LoadTextureFromFile(source, &ret, &tex_manager->texture_metrics[pos]);
-	tex_manager->textures[pos] = ret;
-	tex_manager->cnt++;
+	LoadTextureFromFile(source, &ret, &tex_manager.texture_metrics[pos]);
+	tex_manager.textures[pos] = ret;
+	tex_manager.cnt++;
 	return ret;
 }
 
-void DestroyTextureManager(texture_manager_t* tex_manager) {
-	assert(nullptr != tex_manager);
-	tex_manager->cnt = 0;
-	free(tex_manager->textures);
-	free(tex_manager->texture_metrics);
+void DestroyTextureManager() {
+	tex_manager.cnt = 0;
+	free(tex_manager.textures);
+	free(tex_manager.texture_metrics);
 }
