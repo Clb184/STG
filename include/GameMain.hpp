28eb3ff4f299c3843856f9c35392a29e6032397e
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-10-05 21:18:03 -0500

    Make shaders global

--- include/GameMain.hpp
+++ include/GameMain.hpp
@@ -13,6 +13,8 @@ struct game_main_t {
 	face_manager_t face_manager; // Hold and manager faces (dialogue)
 };
 
+extern struct ShaderData shaders;
+
 bool InitializeGameMain(game_main_t* game_main);
 void DestroyGameMain(game_main_t* game_main);
 
